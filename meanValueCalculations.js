8da88d93f50b380cb8aa9dee38561c2bba8389ed
Love Barrehag <lb224nj@student.lnu.se>
2024-10-02 18:42:09 +0200

    Create logic for harmonic mean vlaue calculations

--- meanValueCalculations.js
+++ meanValueCalculations.js
@@ -15,16 +15,43 @@ class MeanValueCalculations {
     return totalSum
   }
 
-  meanValue (numbers) {
+  /**
+   * Calculates the man value from an array of numbers.
+   */
+  calculateMeanValue (numbers) {
     const totalSum = this.#sumNumbersInArray(numbers)
     return totalSum / numbers.length
   }
 
+  /**
+   * The private method #multiplyNumbersInArray multiplies all numbers in an array and is used in calculateGeometricMeanValue method.
+   */
   #multiplyNumbersInArray (numbers) {
     let totalProduct = 1
     for (let i = 0; i < numbers.length; i++) {
       totalProduct *= numbers[i]
     }
     return totalProduct
+  }
+
+  /**
+   * Calculates the geometric mean value from an array of numbers.
+   */
+  calculateGeometricMeanValue (numbers) {
+    const totalProduct = this.#multiplyNumbersInArray(numbers)
+    return Math.pow(totalProduct, 1 / numbers.length)
+  }
+
+  #sumReciprocalsInArray (numbers) {
+    let totalSumOfReciprocals = 0
+    for (let i = 0; i < numbers.length; i++) {
+      totalSumOfReciprocals += 1 / numbers[i]
+    }
+    return totalSumOfReciprocals
+  }
+
+  calculateHarmonicMeanValue (numbers) {
+    const sumOfReciprocals = this.#sumReciprocalsInArray(numbers)
+    return numbers.length / sumOfReciprocals
   }
 }
