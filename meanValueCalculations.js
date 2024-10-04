ae77af75e3370213c0607fea0bdae2a602a18b78
Love Barrehag <lb224nj@student.lnu.se>
2024-10-04 16:31:11 +0200

    Add validation method and js doc comments

--- meanValueCalculations.js
+++ meanValueCalculations.js
@@ -5,6 +5,17 @@
  */
 class MeanValueCalculations {
   /**
+   * Private method that makes sure the recieved input is an array ant that it contains at least one number.
+   */
+  #checkArraysInput (numbers) {
+    if (!Array.isArray(numbers)) {
+      throw new Error('Input should be an array')
+    }
+    if (numbers.length === 0) {
+      throw new Error('The input must contain one number at a minimum')
+    }
+  }
+  /**
    * The private method #sumNumbersInArray sums all numbers from an array and is used in method meanValue to   calculate the mean value from said array.
    */
   #sumNumbersInArray (numbers) {
@@ -16,9 +27,10 @@ class MeanValueCalculations {
   }
 
   /**
-   * Calculates the man value from an array of numbers.
+   * Calculates the man value from an array of numbers. Uses the private method #checkArraysInput for validation and the private method #sumNumbersInArray to sum all numbers in the array.
    */
   calculateMeanValue (numbers) {
+    this.checkArraysInput(numbers)
     const totalSum = this.#sumNumbersInArray(numbers)
     return totalSum / numbers.length
   }
@@ -35,13 +47,17 @@ class MeanValueCalculations {
   }
 
   /**
-   * Calculates the geometric mean value from an array of numbers.
+   * Calculates the geometric mean value from an array of numbers. Uses the private method #checkArraysInput for validation and the private method #multiplyNumbersInArray to multiply all numbers in the array.
    */
   calculateGeometricMeanValue (numbers) {
+    this.checkArraysInput(numbers)
     const totalProduct = this.#multiplyNumbersInArray(numbers)
     return Math.pow(totalProduct, 1 / numbers.length)
   }
 
+  /**
+   * Sums all reciprocals of all numbers in an array. Is used in calculateHarmonicMeanValue method.
+   */
   #sumReciprocalsInArray (numbers) {
     let totalSumOfReciprocals = 0
     for (let i = 0; i < numbers.length; i++) {
@@ -49,8 +65,11 @@ class MeanValueCalculations {
     }
     return totalSumOfReciprocals
   }
-
+  /**
+   * Calculates the harmonic mean value from an array of numbers. Uses the private method checkArraysInput for validation and the private method #sumReciprocalsInArray to sum all reciprocals in the array.
+   */
   calculateHarmonicMeanValue (numbers) {
+    this.checkArraysInput(numbers)
     const sumOfReciprocals = this.#sumReciprocalsInArray(numbers)
     return numbers.length / sumOfReciprocals
   }
