b4f7fb2835fc107b2403a395febfdac866873952
Love Barrehag <lb224nj@student.lnu.se>
2024-10-07 19:19:29 +0200

    Import / export classes in main file mathOperations, begin testApp to actually start testing if existent logic is successfull

---
+++ mathOperations.js
@@ -0,0 +1,9 @@
+/**
+ * Main file and entry point for users to use the library.
+ * This file exports all the classes available for users to use.
+ */
+import { MeanValueCalculations } from './meanValueCalculations.js'
+import { MedianValueCalculations } from './medianValueCalculations.js'
+
+// Exporting the classes for users to use.
+export { MeanValueCalculations, MedianValueCalculations }
---
+++ testApp.js
@@ -0,0 +1,1 @@
+import { MeanValueCalculations, MedianValueCalculations } from './mathOperations.js'
