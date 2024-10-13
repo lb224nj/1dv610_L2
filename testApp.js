fc61549f6a5c504a52e0049a32a670b9b6e9f072
Love Barrehag <lb224nj@student.lnu.se>
2024-10-13 19:44:41 +0200

    Begin testApp

--- testApp.js
+++ testApp.js
@@ -1,1 +1,8 @@
 import { MeanValueCalculations, MedianValueCalculations } from './mathOperations.js'
+
+const meanValueCalculator = new MeanValueCalculations()
+const medianValueCalculator = new MedianValueCalculations()
+
+const testNumbersForMeanValue = [1, 4, 8, 10, 20, 24]
+const meanValue = meanValueCalculator.calculateMeanValue(testNumbersForMeanValue)
+console.log(`Mean value of [${testNumbersForMeanValue}] is:`, meanValue)
