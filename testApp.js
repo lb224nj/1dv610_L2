fb8e845a4a30b3c20a5aee19a262b9cce355556a
Love Barrehag <lb224nj@student.lnu.se>
2024-10-14 11:42:40 +0200

    Add manual tests for the three types of mean value calculations

--- testApp.js
+++ testApp.js
@@ -3,6 +3,28 @@ import { MeanValueCalculations, MedianValueCalculations } from './mathOperations
 const meanValueCalculator = new MeanValueCalculations()
 const medianValueCalculator = new MedianValueCalculations()
 
-const testNumbersForMeanValue = [1, 4, 8, 10, 20, 24]
+// Test array for manual testing of median value calculations.
+const testNumbersForMeanValue = [4, 8, 12, 16, 20, 24]
+// Test array for manual testing of mean value calculations with a value of 0 included.
+const secondTestNumbersForMeanValue = [4, 8, 12, 16, 20, 24, 0]
+
+// Manual testing of mean value calculations.
 const meanValue = meanValueCalculator.calculateMeanValue(testNumbersForMeanValue)
 console.log(`Mean value of [${testNumbersForMeanValue}] is:`, meanValue)
+
+const secondMeanValue = meanValueCalculator.calculateMeanValue(secondTestNumbersForMeanValue)
+console.log(`Mean value of [${secondTestNumbersForMeanValue}] is:`, secondMeanValue)
+
+// Manual testing of geometric mean value calculations.
+const geometricMeanValue = meanValueCalculator.calculateGeometricMeanValue(testNumbersForMeanValue)
+console.log(`Geometric mean value of [${testNumbersForMeanValue}] is:`, geometricMeanValue)
+
+const secondGeometricMeanValue = meanValueCalculator.calculateGeometricMeanValue(secondTestNumbersForMeanValue)
+console.log(`Geometric mean value of [${secondTestNumbersForMeanValue}] is:`, secondGeometricMeanValue)
+
+// Manual testing of harmonic mean value calculations.
+const harmonicMeanValue = meanValueCalculator.calculateHarmonicMeanValue(testNumbersForMeanValue)
+console.log(`Harmonic mean value of [${testNumbersForMeanValue}] is:`, harmonicMeanValue)
+
+const secondHarmonicMeanValue = meanValueCalculator.calculateHarmonicMeanValue(secondTestNumbersForMeanValue)
+console.log(`Harmonic mean value of [${secondTestNumbersForMeanValue}] is:`, secondHarmonicMeanValue)
