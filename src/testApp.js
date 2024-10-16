1425764c727b0145f9e9bac0c462996a2620fe03
Love Barrehag <lb224nj@student.lnu.se>
2024-10-16 15:56:48 +0200

    Add try catch to testApp for tests to eb able to continue running / displaying in terminal after error occurs

--- src/testApp.js
+++ src/testApp.js
@@ -9,43 +9,63 @@ const testNumbersForMeanValue = [8, 4, 16, 12, 20, 24]
 const secondTestNumbersForMeanValue = [4, 8, 12, 16, 20, 24, 0]
 
 // Manual testing of mean value calculations.
-const meanValue = meanValueCalculator.calculateMeanValue(testNumbersForMeanValue)
-console.log(`Mean value of [${testNumbersForMeanValue}] is:`, meanValue)
+try {
+  const meanValue = meanValueCalculator.calculateMeanValue(testNumbersForMeanValue)
+  console.log(`Mean value of [${testNumbersForMeanValue}] is:`, meanValue)
 
-const secondMeanValue = meanValueCalculator.calculateMeanValue(secondTestNumbersForMeanValue)
-console.log(`Mean value of [${secondTestNumbersForMeanValue}] is:`, secondMeanValue)
+  const secondMeanValue = meanValueCalculator.calculateMeanValue(secondTestNumbersForMeanValue)
+  console.log(`Mean value of [${secondTestNumbersForMeanValue}] is:`, secondMeanValue)
+} catch (error) {
+  console.error('Error in mean value calculations:', error.message)
+}
 
 // Manual testing of geometric mean value calculations.
-const geometricMeanValue = meanValueCalculator.calculateGeometricMeanValue(testNumbersForMeanValue)
-console.log(`Geometric mean value of [${testNumbersForMeanValue}] is:`, geometricMeanValue)
+try {
+  const geometricMeanValue = meanValueCalculator.calculateGeometricMeanValue(testNumbersForMeanValue)
+  console.log(`Geometric mean value of [${testNumbersForMeanValue}] is:`, geometricMeanValue)
 
-const secondGeometricMeanValue = meanValueCalculator.calculateGeometricMeanValue(secondTestNumbersForMeanValue)
-console.log(`Geometric mean value of [${secondTestNumbersForMeanValue}] is:`, secondGeometricMeanValue)
+  const secondGeometricMeanValue = meanValueCalculator.calculateGeometricMeanValue(secondTestNumbersForMeanValue)
+  console.log(`Geometric mean value of [${secondTestNumbersForMeanValue}] is:`, secondGeometricMeanValue)
+} catch (error) {
+  console.error('Error in geometric mean value calculations:', error.message)
+}
 
 // Manual testing of harmonic mean value calculations.
-const harmonicMeanValue = meanValueCalculator.calculateHarmonicMeanValue(testNumbersForMeanValue)
-console.log(`Harmonic mean value of [${testNumbersForMeanValue}] is:`, harmonicMeanValue)
+try {
+  const harmonicMeanValue = meanValueCalculator.calculateHarmonicMeanValue(testNumbersForMeanValue)
+  console.log(`Harmonic mean value of [${testNumbersForMeanValue}] is:`, harmonicMeanValue)
 
-const secondHarmonicMeanValue = meanValueCalculator.calculateHarmonicMeanValue(secondTestNumbersForMeanValue)
-console.log(`Harmonic mean value of [${secondTestNumbersForMeanValue}] is:`, secondHarmonicMeanValue)
+  const secondHarmonicMeanValue = meanValueCalculator.calculateHarmonicMeanValue(secondTestNumbersForMeanValue)
+  console.log(`Harmonic mean value of [${secondTestNumbersForMeanValue}] is:`, secondHarmonicMeanValue)
+} catch (error) {
+  console.error('Error in harmonic mean value calculations:', error.message)
+}
 
 // Test arrays for manual testing of trimmed mean value calculations.
 const testNumbersForTrimmedMeanValue = [8, 4, 16, 12, 20, 24, 50, 60, 20, 30, 12, 15, 18, 22, 25, 28, 32, 35, 38, 42]
 const secondTestNumbersForTrimmedMeanValue = [0, 0, 0, 0, 20, 24, 50, 60, 20, 30, 12, 15, 18, 22, 25, 28, 32, 35, 38, 42, 0, 0, 0, 35, 60, 0, 0, 0, 0]
 
 // Manual testing of trimmed mean value calculations with ten percent.
-const trimmedMeanValueTenPercent = meanValueCalculator.calculateTrimmedMeanValue(testNumbersForTrimmedMeanValue, 10)
-console.log(`Trimmed mean value of [${testNumbersForTrimmedMeanValue}] with 10% trimmed is:`, trimmedMeanValueTenPercent)
+try {
+  const trimmedMeanValueTenPercent = meanValueCalculator.calculateTrimmedMeanValue(testNumbersForTrimmedMeanValue, 10)
+  console.log(`Trimmed mean value of [${testNumbersForTrimmedMeanValue}] with 10% trimmed is:`, trimmedMeanValueTenPercent)
 
-const secondTrimmedMeanValueTenPercent = meanValueCalculator.calculateTrimmedMeanValue(secondTestNumbersForTrimmedMeanValue, 10)
-console.log(`Trimmed mean value of [${secondTestNumbersForTrimmedMeanValue}] with 10% trimmed is:`, secondTrimmedMeanValueTenPercent)
+  const secondTrimmedMeanValueTenPercent = meanValueCalculator.calculateTrimmedMeanValue(secondTestNumbersForTrimmedMeanValue, 10)
+  console.log(`Trimmed mean value of [${secondTestNumbersForTrimmedMeanValue}] with 10% trimmed is:`, secondTrimmedMeanValueTenPercent)
+} catch (error) {
+  console.error('Error in trimmed mean value calculations:', error.message)
+}
 
 // Manual testing of trimmed mean value calculations with forty percent.
-const trimmedMeanValueFortyPercent = meanValueCalculator.calculateTrimmedMeanValue(testNumbersForTrimmedMeanValue, 40)
-console.log(`Trimmed mean value of [${testNumbersForTrimmedMeanValue}] with 40% trimmed is:`, trimmedMeanValueFortyPercent)
+try {
+  const trimmedMeanValueFortyPercent = meanValueCalculator.calculateTrimmedMeanValue(testNumbersForTrimmedMeanValue, 40)
+  console.log(`Trimmed mean value of [${testNumbersForTrimmedMeanValue}] with 40% trimmed is:`, trimmedMeanValueFortyPercent)
 
-const secondTrimmedMeanValueFortyPercent = meanValueCalculator.calculateTrimmedMeanValue(secondTestNumbersForTrimmedMeanValue, 40)
-console.log(`Trimmed mean value of [${secondTestNumbersForTrimmedMeanValue}] with 40% trimmed is:`, secondTrimmedMeanValueFortyPercent)
+  const secondTrimmedMeanValueFortyPercent = meanValueCalculator.calculateTrimmedMeanValue(secondTestNumbersForTrimmedMeanValue, 40)
+  console.log(`Trimmed mean value of [${secondTestNumbersForTrimmedMeanValue}] with 40% trimmed is:`, secondTrimmedMeanValueFortyPercent)
+} catch (error) {
+  console.error('Error in trimmed mean value calculations:', error.message)
+}
 
 // Instance of MedianValueCalculations class, used for manual testing of median value calculations.
 const medianValueCalculator = new MedianValueCalculations()
@@ -56,28 +76,40 @@ const testNumbersForMedianValue = [3, 1, 2, 4, 5]
 const secondTestNumbersForMedianValue = [3, 1, 2, 4, 5, 0]
 
 // Manual testing of median value calculations.
-const medianValue = medianValueCalculator.calculateMedianValue(testNumbersForMedianValue)
-console.log(`Median value of [${testNumbersForMedianValue}] is:`, medianValue)
+try {
+  const medianValue = medianValueCalculator.calculateMedianValue(testNumbersForMedianValue)
+  console.log(`Median value of [${testNumbersForMedianValue}] is:`, medianValue)
 
-const secondMedianValue = medianValueCalculator.calculateMedianValue(secondTestNumbersForMedianValue)
-console.log(`Median value of [${secondTestNumbersForMedianValue}] is:`, secondMedianValue)
+  const secondMedianValue = medianValueCalculator.calculateMedianValue(secondTestNumbersForMedianValue)
+  console.log(`Median value of [${secondTestNumbersForMedianValue}] is:`, secondMedianValue)
+} catch (error) {
+  console.error('Error in median value calculations:', error.message)
+}
 
 // Instance of VarianceValueCalculations class, used for manual testing of variance value calculations.
 const varianceValueCalculator = new VarianceValueCalculations()
 
 // Manual testing of variance value calculations.
-const varianceValue = varianceValueCalculator.calculateVarianceValue(testNumbersForMeanValue)
-console.log(`Variance value of [${testNumbersForMeanValue}] is:`, varianceValue)
+try {
+  const varianceValue = varianceValueCalculator.calculateVarianceValue(testNumbersForMeanValue)
+  console.log(`Variance value of [${testNumbersForMeanValue}] is:`, varianceValue)
 
-const secondVarianceValue = varianceValueCalculator.calculateVarianceValue(secondTestNumbersForMeanValue)
-console.log(`Variance value of [${secondTestNumbersForMeanValue}] is:`, secondVarianceValue)
+  const secondVarianceValue = varianceValueCalculator.calculateVarianceValue(secondTestNumbersForMeanValue)
+  console.log(`Variance value of [${secondTestNumbersForMeanValue}] is:`, secondVarianceValue)
+} catch (error) {
+  console.error('Error in variance value calculations:', error.message)
+}
 
 // Manual testing of standard deviation value calculations.
-const standardDeviationValue = varianceValueCalculator.calculateStandardDeviationValue(testNumbersForMeanValue)
-console.log(`Standard deviation value of [${testNumbersForMeanValue}] is:`, standardDeviationValue)
+try {
+  const standardDeviationValue = varianceValueCalculator.calculateStandardDeviationValue(testNumbersForMeanValue)
+  console.log(`Standard deviation value of [${testNumbersForMeanValue}] is:`, standardDeviationValue)
 
-const secondStandardDeviationValue = varianceValueCalculator.calculateStandardDeviationValue(secondTestNumbersForMeanValue)
-console.log(`Standard deviation value of [${secondTestNumbersForMeanValue}] is:`, secondStandardDeviationValue)
+  const secondStandardDeviationValue = varianceValueCalculator.calculateStandardDeviationValue(secondTestNumbersForMeanValue)
+  console.log(`Standard deviation value of [${secondTestNumbersForMeanValue}] is:`, secondStandardDeviationValue)
+} catch (error) {
+  console.error('Error in standard deviation value calculations:', error.message)
+}
 
 // Instance of ModeValueCalculations class, used for manual testing of mode value calculations.
 const modeValueCalculator = new ModeValueCalculations()
@@ -88,11 +120,15 @@ const testNumbersForModeValue = [4, 1, 4, 4, 2, 3, 2, 4, 3]
 const secondTestNumbersForModeValue = [4, 4, 0, 2, 2, 3, 1, 6, 8, 1]
 
 // Manual testing of mode value calculations.
-const modeValue = modeValueCalculator.calculateModeValue(testNumbersForModeValue)
-console.log(`Mode value of [${testNumbersForModeValue}] is:`, modeValue)
+try {
+  const modeValue = modeValueCalculator.calculateModeValue(testNumbersForModeValue)
+  console.log(`Mode value of [${testNumbersForModeValue}] is:`, modeValue)
 
-const secondModeValue = modeValueCalculator.calculateModeValue(secondTestNumbersForModeValue)
-console.log(`Mode value of [${secondTestNumbersForModeValue}] is:`, secondModeValue)
+  const secondModeValue = modeValueCalculator.calculateModeValue(secondTestNumbersForModeValue)
+  console.log(`Mode value of [${secondTestNumbersForModeValue}] is:`, secondModeValue)
+} catch (error) {
+  console.error('Error in mode value calculations:', error.message)
+}
 
 // Instance of RangeValueCalculations class, used for manual testing of range value calculations.
 const rangeValueCalculator = new RangeValueCalculations()
@@ -103,15 +139,23 @@ const testNumbersForRangeValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15]
 const secondTestNumbersForRangeValue = [-10, -8, -4, -2, 0, 4, 8, 10.5]
 
 // Manual testing of range value calculations.
-const rangeValue = rangeValueCalculator.calculateRangeValue(testNumbersForRangeValue)
-console.log(`Range value of [${testNumbersForRangeValue}] is:`, rangeValue)
+try {
+  const rangeValue = rangeValueCalculator.calculateRangeValue(testNumbersForRangeValue)
+  console.log(`Range value of [${testNumbersForRangeValue}] is:`, rangeValue)
 
-const secondRangeValue = rangeValueCalculator.calculateRangeValue(secondTestNumbersForRangeValue)
-console.log(`Range value of [${secondTestNumbersForRangeValue}] is:`, secondRangeValue)
+  const secondRangeValue = rangeValueCalculator.calculateRangeValue(secondTestNumbersForRangeValue)
+  console.log(`Range value of [${secondTestNumbersForRangeValue}] is:`, secondRangeValue)
+} catch (error) {
+  console.error('Error in range value calculations:', error.message)
+}
 
 // Manual testing of interquartile range value calculations.
-const interquartileRangeValue = rangeValueCalculator.calculateInterquartileRangeValue(testNumbersForRangeValue)
-console.log(`Interquartile range value of [${testNumbersForRangeValue}] is:`, interquartileRangeValue)
+try {
+  const interquartileRangeValue = rangeValueCalculator.calculateInterquartileRangeValue(testNumbersForRangeValue)
+  console.log(`Interquartile range value of [${testNumbersForRangeValue}] is:`, interquartileRangeValue)
 
-const secondInterquartileRangeValue = rangeValueCalculator.calculateInterquartileRangeValue(secondTestNumbersForRangeValue)
-console.log(`Interquartile range value of [${secondTestNumbersForRangeValue}] is:`, secondInterquartileRangeValue)
+  const secondInterquartileRangeValue = rangeValueCalculator.calculateInterquartileRangeValue(secondTestNumbersForRangeValue)
+  console.log(`Interquartile range value of [${secondTestNumbersForRangeValue}] is:`, secondInterquartileRangeValue)
+} catch (error) {
+  console.error('Error in interquartile range value calculations:', error.message)
+}
